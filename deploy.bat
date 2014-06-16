1d257997ed122b413bdecca6ce33f299eba73153
Phil Barr <philbarrmail@gmail.com>
2014-06-16 22:03:33 +0100

    standard commit

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+rem recompile everything
+roots compile
+rem commit all
+git add *
+git commit
+rem push everything for the source
+git push origin master
+rem push everything to the website branch at gh-pages
+git subtree push --prefix public origin gh-pages
