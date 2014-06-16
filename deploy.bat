857f9b7dd432bd778d0310cfa3c26d17f6fa1b88
Phil Barr <philbarrmail@gmail.com>
2014-06-16 22:07:31 +0100

    standard commit

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,6 @@
+rem help git find publickey
+set HOME=%USERPROFILE%
+
 rem recompile everything
 roots compile
 rem commit all
@@ -7,3 +10,4 @@ rem push everything for the source
 git push origin master
 rem push everything to the website branch at gh-pages
 git subtree push --prefix public origin gh-pages
+pause
